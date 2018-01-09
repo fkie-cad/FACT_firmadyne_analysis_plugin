547bff10a7ebccc9be6e6257947e414bc75ad4be
wagner <s6rowagn@uni-bonn.de>
2018-01-09 17:33:29 +0100

    added fping to scan all hosts of ip

--- internal/steps/emulation.py
+++ internal/steps/emulation.py
@@ -10,6 +10,11 @@ def start_emulation(result_dict, emulation_init_time):
     firmware_emulation = start_emulation_process_parallel(emulation_init_time)
     network_accessibility = check_network_accessibility(result_dict['ip'])
     if not network_accessibility:
+        new_ip_address = check_all_host_addresses_and_return_accessible(result_dict['ip'])
+        if new_ip_address:
+            result_dict.update({'emulation': ResultType.SUCCESS})
+            result_dict.update({'ip': new_ip_address})
+            return firmware_emulation
         result_dict.update({'emulation': ResultType.FAILURE, 'error_message': 'Firmadyne wasn\'t able to start the network while emulating'})
         return firmware_emulation
     result_dict.update({'emulation': ResultType.SUCCESS})
@@ -30,6 +35,21 @@ def check_network_accessibility(ip_address):
         return True
     else:
         return False
+
+
+def check_all_host_addresses_and_return_accessible(ip_address):
+    ip_without_host_part = cut_host_part_from_ip(ip_address)
+    output, rc = execute_shell_command_get_return_code('fping -a -q -g {}.0/24'.format(ip_without_host_part))
+    if not output:
+        return output
+    else:
+        logging.debug('new ip with other host address is detected:{}'.format(output))
+        return output.strip()
+
+
+def cut_host_part_from_ip(ip_address):
+    ip_components = ip_address.split('.')
+    return '.'.join(ip_components[:3])
 
 
 def emulate_firmware():
